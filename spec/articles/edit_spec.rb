779c56bdc27ffb1d6a50706b8adc99da8cc084bc
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-01-22 15:39:50 -0700

    add table to index, create Edit and Update methods,

---
+++ spec/articles/edit_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Edit Article', type: :feature do 
+  describe 'when I visit the article show page' do 
+    it 'has a link to edit the article' do 
+      @article_1 = Articles.create(id: 1, title: "second article", description: "ioewajiofnuiewfiopfewahiupewaoijfioweap")
+      
+      visit articles_path
+
+      click_link "edit"
+
+      visit edit_article_path(1)
+
+      fill_in 'Title', with: "third article"
+      fill_in 'Description', with: "zzzfjiewofjieolslslslslslsllssl"
+      click_button "update"
+
+      visit articles_path
+      expect(current_path).to eq(articles_path)
+    end 
+  end 
+end 
