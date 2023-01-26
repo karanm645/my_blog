769e1ad949b513da96910211ceb93e4c24194734
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-01-26 12:17:04 -0700

    Create user table and one to many relation

--- spec/models/articles_spec.rb
+++ spec/models/articles_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Articles, type: :model do
+RSpec.describe Article, type: :model do
   describe 'validations' do 
     it { should validate_presence_of(:title)}
     it { should validate_presence_of(:description)}
@@ -10,5 +10,7 @@ RSpec.describe Articles, type: :model do
    
     it { should validate_length_of(:title).is_at_most(50)}
     it { should validate_length_of(:description).is_at_most(300)}
+  
+    it { should belong_to :user}
   end 
 end 
