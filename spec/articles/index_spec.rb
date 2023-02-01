d636fff01727268261adeb1839379324edb03a4a
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-02-01 11:10:16 -0700

    show article writer and creation date on index

--- spec/articles/index_spec.rb
+++ spec/articles/index_spec.rb
@@ -20,6 +20,16 @@ RSpec.describe "Article Index Page", type: :feature do
       
       expect(page).to have_content(article_1.title)
     end 
+    
+    it 'shows user info in index page' do 
+      user_1 = User.create!(username: "karan443", email: "karanm645@gmail.com")
+      article_1 = Article.create!(title: "1233", description: "fewafewafawefwaf", user_id: user_1.id)
+      
+      visit articles_path
+
+      expect(page).to have_content("Created by: #{user_1.username}")
+
+    end 
 
 
   end 
